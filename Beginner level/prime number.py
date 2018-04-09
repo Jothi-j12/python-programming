a389331957eb27d39ab394a2ca82ef14279514b9
Jothi.J <37336326+Jothi-j12@users.noreply.github.com>
2018-04-09 18:42:49 +0530

    Update prime number.py

--- Beginner level/prime number.py
+++ Beginner level/prime number.py
@@ -1,9 +1,14 @@
 n=int(input())
-if n>1:
-    for i in range(2,n):
-        if n%2==0:
-            print("no")
-        else:
-            print("yes")
+count=0
+for i in range(2,n):
+    if(n%i==0):
+        count=i+1
+if(count!=0):
+    print("no")
+else:
+    print("yes")
+    
+        
+
 
 
