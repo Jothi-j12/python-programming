de177bde7bcfb1cc7f7f0a078c44b52c081cb7c6
Jothi.J <37336326+Jothi-j12@users.noreply.github.com>
2018-04-12 05:51:53 +1300

    Create range of prime numbers.py

---
+++ Beginner level/range of prime numbers.py
@@ -0,0 +1,19 @@
+n=int(input())
+a=int(input())
+count=0
+if(a<n):
+  for i in range(a,n):
+    if(n%i==0):
+      count=i+1
+      if(count!=0):
+        print("no")
+      else:
+        print("yes")
+else(n<a):
+  for i in range(n,a):
+    if(n%i==0):
+        count=i+1
+        if(count!=0):
+          print("no")
+        else:
+          print("yes")
